5e7bb8758fd8c8d097f1971d360fc5f24ced89ff
Shane Jose <90801751+shanejose@users.noreply.github.com>
2023-12-20 14:15:52 -0500

    Add files via upload

---
+++ GameApp.h
@@ -0,0 +1,20 @@
+/**
+ * @file GameApp.h
+ * @author Sanju Kona
+ *
+ *
+ */
+
+#ifndef PROJECT1__GAMEAPP_H
+#define PROJECT1__GAMEAPP_H
+
+/**
+ * Main application class
+ */
+class GameApp : public wxApp {
+public:
+    bool OnInit() override;
+};
+
+
+#endif //PROJECT1__GAMEAPP_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file main.cpp
+ * @author Sanju Kona
+ */
+
+#include "pch.h"
+#include "GameApp.h"
+
+wxIMPLEMENT_APP(GameApp);
